88b5357189fd59ede34aab6c8027824e9dd3ba5d
HuatAhh <tziquanlim@Hotmail.com>
2020-12-07 01:31:33 +0800

    add website files

---
+++ overviewerConfig.js
@@ -0,0 +1,71 @@
+var overviewerConfig = {
+    "CONST": {
+        "tileSize": 384,
+        "image": {
+            "defaultMarker": "signpost.png",
+            "signMarker": "signpost_icon.png",
+            "bedMarker": "bed.png",
+            "spawnMarker": "markers/marker_home.png",
+            "spawnMarker2x": "markers/marker_home_2x.png",
+            "queryMarker": "markers/marker_location.png",
+            "queryMarker2x": "markers/marker_location_2x.png",
+            "compass": {
+                "0": "compass_upper-left.png",
+                "1": "compass_upper-right.png",
+                "3": "compass_lower-left.png",
+                "2": "compass_lower-right.png"
+            }
+        },
+        "mapDivId": "mcmap",
+        "UPPERLEFT": 0,
+        "UPPERRIGHT": 1,
+        "LOWERLEFT": 3,
+        "LOWERRIGHT": 2
+    },
+    "worlds": [
+        "Huat - overworld"
+    ],
+    "map": {
+        "debug": false,
+        "cacheTag": "1607264599",
+        "north_direction": "lower-left",
+        "controls": {
+            "pan": true,
+            "zoom": true,
+            "spawn": true,
+            "compass": true,
+            "mapType": true,
+            "overlays": true,
+            "coordsBox": true
+        }
+    },
+    "tilesets": [
+        {
+            "name": "Daytime",
+            "zoomLevels": 11,
+            "defaultZoom": 1,
+            "maxZoom": 11,
+            "path": "HuatDay",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "Huat - overworld",
+            "last_rendertime": 1607264115,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                165,
+                80,
+                186
+            ],
+            "minZoom": 0,
+            "spawn": [
+                165,
+                80,
+                186
+            ],
+            "north_direction": 0
+        }
+    ]
+};
